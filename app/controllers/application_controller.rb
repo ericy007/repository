7a60cea55ceede31dfbebea2845646721ae7de44
Your Name <you@example.com>
2019-05-18 18:33:20 +0000

    code changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class QuotesController < ApplicationController
-  def index
-  end
+class ApplicationController < ActionController::Base
+
+
 end
--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,5 @@
 class QuotesController < ApplicationController
- def index
- end
-
+  def index
+      @quote = Quote.order("RANDOM()").first
+  end
 end
