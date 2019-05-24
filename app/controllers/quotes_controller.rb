63e8b5fc6970767acf011719595a05f6b03c96c0
Your Name <you@example.com>
2019-05-24 04:36:42 +0000

    code changes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,6 +6,17 @@ class QuotesController < ApplicationController
     def new
       @quote = Quote.new
     end
+
+     def create
+        Quote.create(quote_params)
+        redirect_to root_path
+     end
+
+    private
+
+    def quote_params
+    params.require(:quote).permit(:saying, :author)
+    end
   end
 end
 
