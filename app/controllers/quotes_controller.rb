631b29192f1182c09d745f4be71c7492849218fd
Your Name <you@example.com>
2019-05-25 20:39:54 +0000

    Initial Commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -4,16 +4,16 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
+  
   def create
     Quote.create(quote_params)
      if @quote.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
     redirect_to root_path
+  end
+
+  def about
   end
   
   private
