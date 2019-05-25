af7458058f3766b81fa0e81ef1331cef3cba7306
Your Name <you@example.com>
2019-05-25 19:29:48 +0000

    Initial Commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,6 +5,18 @@ class QuotesController < ApplicationController
   end
 
   def new
+    @quote = Quote.new
+  end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+  
+  private
+
+  def quote_params
+    params.require(:quote).permit(:sying, :author)
   end
 
 end
