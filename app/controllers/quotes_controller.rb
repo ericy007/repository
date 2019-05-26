40750fa1894a0bdc9832b210c2368f485748e160
Your Name <you@example.com>
2019-05-26 05:23:37 +0000

    Initial Commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,11 +5,11 @@ class QuotesController < ApplicationController
   end
 
   
-  def create
+ def create
     @quote = Quote.create(quote_params)
-     if @quote.invalid?
-    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-  end
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
     redirect_to root_path
   end
 
