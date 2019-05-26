5bf334033de2b9b5f9ac9a314d8e10d4ef30005d
Your Name <you@example.com>
2019-05-26 04:51:49 +0000

    Initial Commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,7 +6,7 @@ class QuotesController < ApplicationController
 
   
   def create
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
      if @quote.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
